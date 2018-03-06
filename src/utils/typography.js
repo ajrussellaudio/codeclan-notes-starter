f6807b6a281afc610a83499dca270c8231ff00fe
Alan Russell <ajrussellaudio@gmail.com>
2018-03-06 10:33:21 +0000

    adjust text style

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,19 +1,77 @@
 import Typography from 'typography'
-import Wordpress2016 from 'typography-theme-wordpress-2016'
+import gray from 'gray-percentage'
+import { TABLET_MEDIA_QUERY } from 'typography-breakpoint-constants'
 
-Wordpress2016.overrideThemeStyles = () => ({
-  'a.gatsby-resp-image-link': {
-    boxShadow: 'none',
-  },
-})
-
-delete Wordpress2016.googleFonts
-
-const typography = new Typography(Wordpress2016)
-
-// Hot reload typography in development.
-if (process.env.NODE_ENV !== 'production') {
-  typography.injectStyles()
+const colors = {
+  darkblue: '#2372B5',
+  lightblue: '#3DA9DD',
+  black: '#3C3C3B',
+  white: '#FFFFFF',
 }
+
+const typography = new Typography({
+  title: 'Russell',
+  baseFontSize: '21px',
+  baseLineHeight: 1.3,
+  scaleRatio: 1.5,
+  googleFonts: [
+    {
+      name: 'Dosis',
+      styles: ['800', '600', '400'],
+    },
+    {
+      name: 'Open Sans',
+      styles: ['400', '400i', '700'],
+    },
+    {
+      name: 'Source Code Pro',
+      styles: ['400'],
+    },
+  ],
+  headerFontFamily: ['Dosis', 'sans-serif'],
+  bodyFontFamily: ['Open Sans', 'sans-serif'],
+  bodyColor: colors.black,
+  headerWeight: 600,
+  bodyWeight: 400,
+  boldWeight: 700,
+  overrideStyles: ({ adjustFontSizeTo, scale, rhythm }, options) => ({
+    a: {
+      color: colors.darkblue,
+    },
+    'a:hover': {
+      color: colors.lightblue,
+    },
+    blockquote: {
+      ...scale(1 / 5),
+      color: gray(30),
+      fontStyle: 'italic',
+      paddingLeft: rhythm(3 / 4),
+      marginLeft: 0,
+      marginRight: 0,
+      borderLeft: `${rhythm(1 / 4)} solid ${options.bodyColor}`,
+    },
+    code: {
+      ...adjustFontSizeTo('16px'),
+      fontFamily: '"Source Code Pro", mono',
+    },
+    'h1,h2,h3,h4,h5,h6': {
+      // textTransform: 'lowercase',
+      paddingBottom: rhythm(1 / 4),
+      marginBottom: rhythm(1 / 4),
+      color: colors.darkblue,
+    },
+    table: {
+      ...scale(-1 / 5),
+    },
+    // Mobile styles.
+    [TABLET_MEDIA_QUERY]: {
+      blockquote: {
+        marginLeft: rhythm(-3 / 4),
+        marginRight: 0,
+        paddingLeft: rhythm(1 / 2),
+      },
+    },
+  }),
+})
 
 export default typography
