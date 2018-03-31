def48b55ad18788e634174a27ce38fc699977bbc
Alan Russell <ajrussellaudio@gmail.com>
2018-03-31 20:23:35 +0100

    display lessons in drop-down carousel by week and day

--- src/components/LessonList.js
+++ src/components/LessonList.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import get from 'lodash/get'
+import groupBy from 'lodash/groupBy'
 
 import LessonHeader from './LessonHeader'
 import LessonDescription from './LessonDescription'
@@ -14,19 +15,41 @@ const lessonIsBeforeToday = (lesson, thisWeek, today) => {
   )
 }
 
-const LessonList = ({ lessons, thisWeek, today }) => {
+const DailyList = ({ lessons }) => {
+  const days = groupBy(lessons, 'node.frontmatter.day')
   return (
     <div>
-      {lessons
-        .filter(({ node }) => lessonIsBeforeToday(node, thisWeek, today))
-        .map(({ node }) => (
-          <div key={node.fields.slug}>
-            <LessonHeader {...node} />
-            <LessonDescription {...node} />
-          </div>
-        ))}
+      {Object.keys(days).map(dayNumber => (
+        <details key={dayNumber}>
+          <summary>Day {dayNumber}</summary>
+          {days[dayNumber].map(({ node }) => (
+            <div key={node.fields.slug}>
+              <LessonHeader {...node} />
+              <LessonDescription {...node} />
+            </div>
+          ))}
+        </details>
+      ))}
     </div>
   )
+}
+
+const WeeklyList = ({ lessons }) => {
+  const weeks = groupBy(lessons, 'node.frontmatter.week')
+  return (
+    <div>
+      {Object.keys(weeks).map(weekNumber => (
+        <details key={weekNumber}>
+          <summary>Week {weekNumber}</summary>
+          <DailyList lessons={weeks[weekNumber]} />
+        </details>
+      ))}
+    </div>
+  )
+}
+
+const LessonList = ({ lessons, thisWeek, today }) => {
+  return <WeeklyList lessons={lessons} />
 }
 
 export default LessonList
