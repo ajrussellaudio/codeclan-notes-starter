4dc58e67bcee799ddefb8ce9787fea6e8cb92bcd
Alan Russell <ajrussellaudio@gmail.com>
2018-04-01 13:35:22 +0100

    make lesson index sfc

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,55 +5,53 @@ import Helmet from 'react-helmet'
 import Bio from '../components/Bio'
 import LessonList from '../components/LessonList'
 
-class BlogIndex extends React.Component {
-  render() {
-    const siteTitle = get(this, 'props.data.site.siteMetadata.title')
+const LessonIndex = props => {
+  const siteTitle = get(props, 'data.site.siteMetadata.title')
 
-    const description = ({ frontmatter, excerpt }) => {
-      if (frontmatter.objectives) {
-        return (
-          <ul>
-            {frontmatter.objectives.map((objective, i) => (
-              <li key={i}>{objective}</li>
-            ))}
-          </ul>
-        )
-      } else {
-        return <p dangerouslySetInnerHTML={{ __html: excerpt }} />
-      }
-    }
-
-    const lessonsBeforeToday = get(
-      this,
-      'props.data.allMarkdownRemark.edges',
-      []
-    ).filter(lesson => {
-      console.log(lesson)
-      const lessonWeek = get(lesson, 'node.frontmatter.week')
-      const lessonDay = get(lesson, 'node.frontmatter.day')
-      const thisWeek = get(this, 'props.data.site.siteMetadata.date.week', 1)
-      const today = get(this, 'props.data.site.siteMetadata.date.day', 1)
-
+  const description = ({ frontmatter, excerpt }) => {
+    if (frontmatter.objectives) {
       return (
-        // lesson comes before this week, or
-        // lesson comes THIS week, on or before today
-        lessonWeek < thisWeek || (lessonWeek === thisWeek && lessonDay <= today)
+        <ul>
+          {frontmatter.objectives.map((objective, i) => (
+            <li key={i}>{objective}</li>
+          ))}
+        </ul>
       )
-    })
+    } else {
+      return <p dangerouslySetInnerHTML={{ __html: excerpt }} />
+    }
+  }
+
+  const lessonsBeforeToday = get(
+    props,
+    'data.allMarkdownRemark.edges',
+    []
+  ).filter(lesson => {
+    console.log(lesson)
+    const lessonWeek = get(lesson, 'node.frontmatter.week')
+    const lessonDay = get(lesson, 'node.frontmatter.day')
+    const thisWeek = get(props, 'data.site.siteMetadata.date.week', 1)
+    const today = get(props, 'data.site.siteMetadata.date.day', 1)
 
     return (
-      <div>
-        <Helmet title={siteTitle}>
-          <link rel="icon" type="image/png" href="/favicon.png" />
-        </Helmet>
-        <Bio />
-        <LessonList lessons={lessonsBeforeToday} />
-      </div>
+      // lesson comes before this week, or
+      // lesson comes THIS week, on or before today
+      lessonWeek < thisWeek || (lessonWeek === thisWeek && lessonDay <= today)
     )
-  }
+  })
+
+  return (
+    <div>
+      <Helmet title={siteTitle}>
+        <link rel="icon" type="image/png" href="/favicon.png" />
+      </Helmet>
+      <Bio />
+      <LessonList lessons={lessonsBeforeToday} />
+    </div>
+  )
 }
 
-export default BlogIndex
+export default LessonIndex
 
 export const pageQuery = graphql`
   query IndexQuery {
