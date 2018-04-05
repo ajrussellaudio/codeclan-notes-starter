f9676d5ce05f935cd155b70cc289dd2ea6d36c6f
Alan Russell <ajrussellaudio@gmail.com>
2018-04-05 16:33:02 +0100

    Updates

---
+++ path---index-f892064325e625f1d4bb.js
@@ -0,0 +1,2 @@
+webpackJsonp([0x81b8806e4260],{508:function(t,e){t.exports={data:{site:{siteMetadata:{title:"CodeClan",date:{week:2,day:1}}},allMarkdownRemark:{edges:[{node:{excerpt:"Introduction This project delivers our written notes to students. Notes are converted automatically from our  .md  Markdown files into rich…",fields:{slug:"/tutorial/"},frontmatter:{title:"How to use this thing",objectives:["A tutorial in using this method of delivering student notes"],week:0,day:0,order:0}}}]}},pathContext:{}}}});
+//# sourceMappingURL=path---index-f892064325e625f1d4bb.js.map
